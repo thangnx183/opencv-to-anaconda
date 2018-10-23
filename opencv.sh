726f1fba23252a73dca05493b54faab305f8eede
thangnx183 <nxthang1831997@gmail.com>
2018-10-23 22:52:01 +0700

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -1,29 +1,29 @@
  sudo apt-get update
-$ sudo apt-get upgrade
-$ sudo apt-get install build-essential cmake git pkg-config
-$ sudo apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
-$ sudo apt-get install libgtk2.0-dev
-$ sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-$ sudo apt-get install libatlas-base-dev gfortran
-$ sudo apt-get install python2.7-dev
+ sudo apt-get upgrade
+ sudo apt-get install build-essential cmake git pkg-config
+ sudo apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
+ sudo apt-get install libgtk2.0-dev
+ sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+ sudo apt-get install libatlas-base-dev gfortran
+ sudo apt-get install python2.7-dev
 
 # clone source
-$ cd ~
-$ git clone https://github.com/Itseez/opencv.git
-$ cd opencv
-$ git checkout 3.0.0
+ cd ~
+ git clone https://github.com/Itseez/opencv.git
+ cd opencv
+ git checkout 3.0.0
 
 # SIFT SURF are in opencv_contrib.git
-$ cd ~
-$ git clone https://github.com/Itseez/opencv_contrib.git
-$ cd opencv_contrib
-$ git checkout 3.0.0
+ cd ~
+ git clone https://github.com/Itseez/opencv_contrib.git
+ cd opencv_contrib
+ git checkout 3.0.0
 
 # 
-$ cd ~/opencv
-$ mkdir build
-$ cd build
-$ cmake -DCMAKE_BUILD_TYPE=RELEASE \
+ cd ~/opencv
+ mkdir build
+ cd build
+ cmake -DCMAKE_BUILD_TYPE=RELEASE \
  -DOPENCV_EXTRA_MODULES_PATH=~/github/opencv_contrib/modules \
  -DWITH_TBB=ON \
  -DBUILD_NEW_PYTHON_SUPPORT=ON \
@@ -36,22 +36,23 @@ $ cmake -DCMAKE_BUILD_TYPE=RELEASE \
  -DPYTHON_EXECUTABLE=$(which python) \
  -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
  -DPYTHON_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
+ -DCMAKE_INSTALL_PREFIX=/usr/local \
 ..
 
 # -DCMAKE_INSTALL_PREFIX=/usr/local \ #specify any location you want to install
 
-$ make -j4
-$ sudo make install
-$ sudo ldconfig
+ make -j4
+ sudo make install
+ sudo ldconfig
 
 # check if .so are installed in your specified location
 # ex. cv2.so should be installed into
 # /home/wew57/anaconda/lib/python2.7/site-packages/cv2.so
 
-$ python
->>> import cv2
->>> cv2.__version__
-'3.0.0' # is it the version you installed
+# python
+#>>> import cv2
+#>>> cv2.__version__
+#'3.0.0' # is it the version you installed
 
 # taste demo 
-opencv/samples/python2/demo.py
+#opencv/samples/python2/demo.py
